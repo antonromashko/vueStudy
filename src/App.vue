cc0f0163563671415682ffacea210521f04caf67
Anton Romashko <anton.romashko@pm.bet>
2020-12-08 12:24:32 +0200

    hw3: task1

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,29 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+      <CustomSelect  :options-list="optionsList"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import CustomSelect from '@/components/CustomSelect.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    CustomSelect
+  },
+  data() {
+    return {
+      optionsList: [
+        { label: 'select 1', id: '1' },
+        { label: 'select 2', id: '2' },
+        { label: 'select 3', id: '3' }
+      ]
+    }
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="scss" scoped>
+
 </style>
---
+++ src/components/CustomSelect.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="custom-select">
+    <div class="option default-option" @click="openSelect">{{ defaultOption }} <span>&#9662;</span></div>
+    <template v-if="opened">
+      <div v-for="(option, idx) of optionsList" :key="idx" class="option" @click="selectOption(option)">
+      {{ option.label }}
+      </div>
+    </template>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CustomSelect",
+  props: {
+    optionsList: {
+      required: true,
+      type: Array
+    },
+    defaultOption: {
+      type: Text,
+      default: 'choose'
+    }
+  },
+  data() {
+    return {
+      opened: false
+    }
+  },
+  methods: {
+    openSelect() {
+      this.opened = !this.opened
+    },
+    selectOption(option) {
+      this.defaultOption = option.label
+      this.opened = false
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  .custom-select {
+    display: flex;
+    flex-direction: column;
+    width: fit-content;
+    padding: 10px;
+    font-size: 20px;
+    font-weight: 500;
+    .option {
+      padding: 5px;
+      position: relative;
+      border-width: 0 1px 1px 1px;
+      border-style: solid;
+      border-color: rgba(3, 3, 3, 0.12);
+    }
+    .default-option {
+      border-width: 1px;
+      background: #f1f1f1;
+    }
+    .option:hover {
+      background: #f1f1f1;
+    }
+    .default-option:hover {
+      background: #e0e0e0;
+    }
+  }
+</style>
